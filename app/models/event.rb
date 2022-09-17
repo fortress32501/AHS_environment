ab5fc40fa27fa32f9f1870e9bc6c7cccfc99f985
caleb-terry17 <k.cterry30@gmail.com>
2022-09-17 00:53:37 -0500

    updated how points are validated

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ApplicationRecord
     validates :title, presence: true 
     validates :section, presence: true
     validates :schedule, presence: true
-    validates :points, presence: true, numericality: {min: 0}
+    validates :points, presence: true, numericality: {greater_than_or_equal_to: 0}
     validates :passcode, presence: true
 end
