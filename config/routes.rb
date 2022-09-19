28973dbd66e190898e864cff801fc50b67e9f6ce
root <root@localhost>
2022-09-19 02:03:28 +0000

    make login page to be root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root to: "sessions#new"
   resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root "events#index"
