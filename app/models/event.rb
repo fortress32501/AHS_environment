5fc00aee9d24791bbda42ffd1695123ecae815b0
rdoer <93722146+rdoer@users.noreply.github.com>
2022-09-18 21:53:33 -0500

    Updated event.rb to make attribute names match

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,5 @@
 class Event < ApplicationRecord
-    validates :title, presence: true 
-    validates :section, presence: true
-    validates :schedule, presence: true
-    validates :points, presence: true, numericality: {greater_than_or_equal_to: 0}
-    validates :passcode, presence: true
+    validates :event_description, :event_passcode, :event_start, :event_end, :event_title, :event_location, presence: true     
+    validates :event_points, presence: true, numericality: {greater_than_or_equal_to: 0}
+
 end
