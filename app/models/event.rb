e6f499f26f03e15dea459ca5b8ec8abec30f4a88
unknown <joshuahillx1@gmail.com>
2022-09-26 00:11:20 -0500

    simple_calendar framework added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
     validates :event_points, :event_description, :event_passcode, :event_start, :event_end, :event_title, :event_location, presence: true
+
+    #def self.start_time
+    #    :event_start
+    #end
 end
