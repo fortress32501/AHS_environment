87d018881cf6ec54039366e6c9ebd2fefbbdf432
root <root@localhost>
2022-10-06 22:34:12 +0000

    moved sql command to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 class User < ApplicationRecord
     has_secure_password
     validates :first_name, :last_name, :email, presence: true
+    # has_one :ranking_id
     # https://medium.com/@rmeji1/creating-a-login-with-simple-auth-using-ruby-on-rails-7dd95a03cb7a
     def welcome
     "Hello, #{self.first_name} #{self.last_name} !"
