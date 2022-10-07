a78c8dc2d269fe03257cd81591da1a4fb599fdc9
caleb-terry17 <k.cterry30@gmail.com>
2022-10-07 10:58:07 -0500

    added factory girl for creating test data

--- spec/views/admin_requests/edit.html.erb_spec.rb
+++ spec/views/admin_requests/edit.html.erb_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "admin_requests/edit", type: :view do
   before(:each) do
+    @user = assign(:user, User.create!(
+        id: 1,
+        first_name: "testUser",
+        last_name: "1",
+        email: "testUser1@email.com",
+        password: "testUser1!Hello",
+        point: 0,
+        is_admin: false
+    ))
+
     @admin_request = assign(:admin_request, AdminRequest.create!(
       user_id: 1,
       request_status: "MyString"
