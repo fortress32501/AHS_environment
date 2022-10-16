1b056d1ce9e05168fe08b1f3bf6fc9903cce0ead
caleb-terry17 <k.cterry30@gmail.com>
2022-10-16 17:46:52 -0500

    fixed error on test case for show

--- spec/views/admin_requests/show.html.erb_spec.rb
+++ spec/views/admin_requests/show.html.erb_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe "admin_requests/show", type: :view do
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/2/)
+    expect(rendered).to match(@test_user.id.to_s)
     expect(rendered).to match(/Request Status/)
   end
 end
