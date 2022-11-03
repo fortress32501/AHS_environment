e282f1c9a74fa7ebf5c93b1599df4511f26814ea
root <root@localhost>
2022-11-03 21:08:52 +0000

    add admin request and improve account styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
   before_action :set_user, only: %i[ show edit update destroy ]
-  layout 'login_signup', only: :new
+  layout 'screen_size', only: :new
   
   # GET /users/1 or /users/1.json
   def show
