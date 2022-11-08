9cc2050bd3c5193db9512aab6558e192bdf6bfb8
root <root@localhost>
2022-11-08 03:39:30 +0000

    Show attendees/ fix login page/ improve account page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,26 @@
 class Event < ApplicationRecord
-	belongs_to :event_type, :optional => true
-    has_many :users, through: :attendance 
-    validates :event_points, :event_description, :event_passcode, :event_start, :event_end, :event_title, :event_location, presence: true
+  belongs_to :event_type, :optional => true
+  has_many :users, through: :attendance 
+  validates :event_points, :event_description, :event_passcode, :event_start, :event_end, :event_title, :event_location, presence: true
+  
+  # find attendance for current event
+  def attendees
+    Attendance.where(:event_id => self.id).all
+  end
+  
+  # find user first and last name by id
+  def find_name(user_id)
+    User.find(user_id).last_name + ", " +User.find(user_id).first_name
+  end
+  
+  # show 3 recent activities on account page
+  def recent_events
+    Event.order('event_start DESC').limit(3)
+  end
+
 end
+
+
+
+
+    
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+
 class User < ApplicationRecord
   has_secure_password
   has_many :events, through: :attendance
@@ -20,17 +21,22 @@ class User < ApplicationRecord
       "Member"
     end
   end
+  
+  # show 3 recent attendance records
+  def recent_attendance
+    self.attendance_history.limit(3)
+  end
 
-  # join attendance table with event table
+  # join attendance table with event table on current user
   def attendance_history
-    @attendance_history = Attendance.joins(:event).where(attendances: { user_id: self.id }).order('event_start DESC')
+    Attendance.joins(:event).where(attendances: { user_id: self.id }).order('event_start DESC')
   end
 
   # calculate attendance point of user
   def user_points
-    @user_points = self.attendance_history.sum(:event_points)
+    self.attendance_history.sum(:event_points)
   end
-
+  
   def assign_ranking
     # ranking_found = Ranking.where("point_total <= #{self.point}").order(point_total: :desc)
     ranking_found = Ranking.where("point_total <= ?", self.point ).order(point_total: :desc)
@@ -41,7 +47,7 @@ class User < ApplicationRecord
       # update ranking
       self.update(ranking_id: ranking_found.ids.at(0))
     end
-    # "#{self.ranking_id}"
+    # "Ranking is : #{self.ranking_id} !!"
   end
   
   # https://stackoverflow.com/questions/45252984/how-to-update-specific-column-in-a-activerecord-on-rails
@@ -63,7 +69,6 @@ class User < ApplicationRecord
   def get_ranking_title
     # ranking_found = Ranking.where("point_total <= #{self.point}").order(point_total: :desc)
     # SELECT title FROM Rankings Join Users On Rankings.id=2;
-
     title_found = Ranking.where(id: self.ranking_id).first
     if title_found == nil
       # nothing to do
@@ -72,7 +77,7 @@ class User < ApplicationRecord
     end
   end
 
-  def show_ranking_title(value)
+  def show_title(value)
     # ranking_found = Ranking.where("point_total <= #{self.point}").order(point_total: :desc)
     # SELECT title FROM Rankings Join Users On Rankings.id=2;
     title_found = Ranking.where(id: value).first
