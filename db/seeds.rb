fc35ddf936fd0c6917e62bf877eaeb9d7c6d8be1
root <root@localhost>
2022-11-10 20:42:05 +0000

    coment out seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,22 +12,22 @@
 # User.create(first_name: "John", last_name: "Jones", email: "john@email.com", password: "123456", point: "500", is_admin: true)
 # User.create(first_name: "James", last_name: "White", email: "james@email.com", password: "1234", point: "800", is_admin: false)
 
-Ranking.create(title: "Diamond", point_total: "1000")
-Ranking.create(title: "Gold", point_total: "700")
-Ranking.create(title: "Silver", point_total: "500")
-Ranking.create(title: "Bronze", point_total: "100")
-Ranking.create(title: "Iron", point_total: "0")
+# Ranking.create(title: "Diamond", point_total: "1000")
+# Ranking.create(title: "Gold", point_total: "700")
+# Ranking.create(title: "Silver", point_total: "500")
+# Ranking.create(title: "Bronze", point_total: "100")
+# Ranking.create(title: "Iron", point_total: "0")
 
-Event.create!([
-  {event_points: 1, event_description: "Event 1 Description", event_passcode: "password", event_start: "2022-10-07 16:22:00", event_end: "2022-10-07 16:22:00", event_title: "Event 1", event_location: "Zoom", event_type_id: "1"},
-  {event_points: 2, event_description: "Event 2 Description", event_passcode: "password2", event_start: "2022-10-08 16:23:00", event_end: "2022-10-08 16:23:00", event_title: "Event 2", event_location: "Zoom", event_type_id: ""}
-])
-EventType.create!([
-  {type_name: "Event Type 1", description: "Event Description 1", color: "#0400f5"}
-])
-User.create!([
-  {first_name: "Test", last_name: "Test", email: "test@gmail.com", password_digest: "$2a$12$7wDlUndiAVVpEMSStsZkA./R8PMWYE3okaP7gVJkSXI00vdslmQZG", point: 0, is_admin: true, ranking_id: nil}
-])
-User.create!([
-  {first_name: "Pear", last_name: "Ratano", email: "pear@gmail.com", password: "123", point: 0, is_admin: false, ranking_id: nil}
-])
+# Event.create!([
+#   {event_points: 1, event_description: "Event 1 Description", event_passcode: "password", event_start: "2022-10-07 16:22:00", event_end: "2022-10-07 16:22:00", event_title: "Event 1", event_location: "Zoom", event_type_id: "1"},
+#   {event_points: 2, event_description: "Event 2 Description", event_passcode: "password2", event_start: "2022-10-08 16:23:00", event_end: "2022-10-08 16:23:00", event_title: "Event 2", event_location: "Zoom", event_type_id: ""}
+# ])
+# EventType.create!([
+#   {type_name: "Event Type 1", description: "Event Description 1", color: "#0400f5"}
+# ])
+# User.create!([
+#   {first_name: "Test", last_name: "Test", email: "test@gmail.com", password_digest: "$2a$12$7wDlUndiAVVpEMSStsZkA./R8PMWYE3okaP7gVJkSXI00vdslmQZG", point: 0, is_admin: true, ranking_id: nil}
+# ])
+# User.create!([
+#   {first_name: "Pear", last_name: "Ratano", email: "pear@gmail.com", password: "123", point: 0, is_admin: false, ranking_id: nil}
+# ])
